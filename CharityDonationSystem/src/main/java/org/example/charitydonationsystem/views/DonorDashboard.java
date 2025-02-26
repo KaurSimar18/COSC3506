49e5a5be458d84e1381cb153c84e0396c03d44ba
Lovepreet Singh <lovepresingh@algomau.ca>
2025-02-26 08:19:21 -0400

    Added DBUtil and Donor Views

---
+++ CharityDonationSystem/src/main/java/org/example/charitydonationsystem/views/DonorDashboard.java
@@ -0,0 +1,29 @@
+package org.example.charitydonationsystem.views;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.example.charitydonationsystem.components.Sidebar;
+
+public class DonorDashboard {
+
+    public void show(Stage stage, int userId) {
+        Label welcome = new Label("Donor Dashboard");
+        welcome.setStyle("-fx-font-size: 18px;");
+
+        VBox content = new VBox(20, welcome);
+        content.setStyle("-fx-padding: 30; -fx-alignment: center;");
+
+        BorderPane root = new BorderPane();
+        root.setLeft(Sidebar.createSidebar(stage, "donor", userId));
+        root.setCenter(content);
+
+        Scene scene = new Scene(root, 800, 500);
+        stage.setScene(scene);
+        stage.setTitle("Donor Dashboard");
+    }
+}
+
+
